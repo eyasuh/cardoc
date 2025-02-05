315671853a734045d329e19309b742a14d4da234
eyasuh <eyha12@gmail.com>
2025-02-05 00:51:40 -0500

    Initial commit

---
+++ src/main/java/com/hulum/cardoc/CarDocApplication.java
@@ -0,0 +1,13 @@
+package com.hulum.cardoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hulum/cardoc/CarDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hulum.cardoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
