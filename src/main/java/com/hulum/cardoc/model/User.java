5f24b93ce4b19b607e4053d945c1b02186609321
eyasuh <eyha12@gmail.com>
2025-02-05 20:16:15 -0500

    added user model and update app test

---
+++ src/main/java/com/hulum/cardoc/model/User.java
@@ -0,0 +1,4 @@
+package com.hulum.cardoc.model;
+
+public record User() {
+}
--- src/test/java/com/hulum/cardoc/CarDocApplicationTests.java
+++ src/test/java/com/hulum/cardoc/CarDocApplicationTests.java
@@ -1,10 +1,14 @@
 package com.hulum.cardoc;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CarDocApplicationTests {
+	@Autowired
+	TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
